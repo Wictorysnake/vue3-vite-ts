7325fcba626c4bb9c50c4b0c9e253f42944eaf6d
wictory <chenyi0195@163.com>
2021-12-09 15:00:03 +0800

    initial commit

---
+++ .prettier.js
@@ -0,0 +1,22 @@
+module.exports = {
+  tabWidth: 2,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+  printWidth: 100,
+  singleQuote: true,
+  semi: false,
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
+  arrowParens: "always",
+};
+
+//   作者：前端开发爱好者
+//   链接：https://zhuanlan.zhihu.com/p/441467856
+//   来源：知乎
+//   著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
