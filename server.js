4f1f23b1776c7c0d2e2b273bd78864f31f49a0d9
wictory <chenyi0195@163.com>
2021-12-17 10:29:32 +0800

    feat(server.js): 修改登录接口

--- server.js
+++ server.js
@@ -4,20 +4,33 @@ const port = 3000
 http
   .createServer((req, res) => {
     console.log(req)
-    const { url } = req
+    const { url, method } = req
 
-    if (url === '/login') {
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      res.end(
-        JSON.stringify({
-          code: 0,
-          message: '成功',
-          data: {
-            userName: 'zhangsan',
-            passWord: '123456',
-          },
+    if (url === '/login' && method === 'POST') {
+      let data = ''
+      req.on('data', (chunk) => {
+        data += chunk
+      })
+
+      req.on('end', () => {
+        // 解码，可能会有中文
+        data = decodeURI(data)
+        console.log(data)
+
+        const dataObj = JSON.parse(data)
+        console.log(dataObj)
+
+        res.writeHead(200, {
+          'Content-Type': 'application/json',
         })
-      )
+        res.end(
+          JSON.stringify({
+            code: 0,
+            message: '成功',
+            data: dataObj,
+          })
+        )
+      })
     } else {
       res.end('Hello World')
     }
